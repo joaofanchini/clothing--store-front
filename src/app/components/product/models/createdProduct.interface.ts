4689f5d52f3e9bc9dee08c2bbc76349aab268332
Vinicius Nascimento <vini.nasc@gmail.com>
2020-04-07 18:46:11 -0300

    Interface de produtos

---
+++ src/app/components/product/models/createdProduct.interface.ts
@@ -0,0 +1,13 @@
+export interface CreatedProduct {
+  createdDate?: string;
+  _id?: string;
+  type?: string;
+  brand?: string;
+  characteristics?: string;
+  size?: string;
+  color?: string;
+  purchaseTagValue?: string;
+  amountPaidOnPurchase?: string;
+  valueForMargin?: string;
+  suggestedPrice?: string;
+}
